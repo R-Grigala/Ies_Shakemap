034f2533d97cf4fbb690ac369f18bbb086eb67a2
ies <smc@iliauni.edu.ge>
2026-08-05 11:59:18 +0000

    Update ShakeMap service

--- src/services/calc_shakemap.py
+++ src/services/calc_shakemap.py
@@ -30,7 +30,7 @@ def calc_shakemap(parsed_data):
     )
     logger.info("Running ShakeMap for event %s with command: %s", event_id, sm_create_cmd)
 
-    shake_cmd = f'echo {event_id} | shake {event_id} select assemble model contour mapping'
+    shake_cmd = f'echo {event_id} | shake {event_id} select assemble model contour mapping kml'
 
     bash_command = (
         f'eval "$({shlex.quote(conda_exe)} shell.bash hook)" '
